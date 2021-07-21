f67aca473f87b425021cab1add052fcbd2899e58
ericbsantana <bitencourteric@hotmail.com>
2021-07-21 11:26:50 -0300

    change toast config

--- src/helpers/notify.js
+++ src/helpers/notify.js
@@ -5,13 +5,5 @@ export const ToastClear = () => {
 };
 
 export const ToastError = (msg) => {
-  toast.error(msg, {
-    position: "top-right",
-    autoClose: false,
-    hideProgressBar: true,
-    closeOnClick: true,
-    pauseOnHover: true,
-    draggable: true,
-    progress: undefined,
-  });
+  toast.error(msg);
 };
