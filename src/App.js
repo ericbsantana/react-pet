468d3609411699f9ab4fcb561da1bc4000825b42
ericbsantana <bitencourteric@hotmail.com>
2021-07-25 19:23:41 -0300

    add userdetails page

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import Login from "./views/LoginView";
 import Signup from "./views/Signup";
 import NotFound from "./views/NotFound";
 import PetDetails from "./views/PetDetails";
+import UserDetails from "./views/UserDetails";
 import AddPet from "./views/AddPet";
 
 import PrivateRoute from "./routes/PrivateRoute";
@@ -30,34 +31,17 @@ function App() {
           <Navbar />
           <div className="sm:container mx-auto">
             <Switch>
-              <PublicRoute path="/about">
-                <About />
-              </PublicRoute>
-              <PrivateRoute component={AddPet} path="/add" />
-              <PublicRoute path="/pets/:id">
-                <PetDetails />
-              </PublicRoute>
-              <PublicRoute path="/pets">
-                <Pets />
-              </PublicRoute>
-              <PublicRoute path="/search">
-                <Search />
-              </PublicRoute>
-              <PublicRoute path="/users">
-                <Users />
-              </PublicRoute>
-              <PublicRoute path="/login">
-                <Login />
-              </PublicRoute>
-              <PublicRoute path="/signup">
-                <Signup />
-              </PublicRoute>
-              <PublicRoute exact path="/">
-                <Home />
-              </PublicRoute>
-              <PublicRoute>
-                <NotFound />
-              </PublicRoute>
+              <PublicRoute path="/about" component={About} />
+              <PrivateRoute path="/add" component={AddPet} />
+              <PublicRoute path="/pets/:id" component={PetDetails} />
+              <PublicRoute path="/pets" component={Pets} />
+              <PublicRoute path="/search" component={Search} />
+              <PublicRoute path="/users/:id" component={UserDetails} />
+              <PublicRoute path="/users" component={Users} />
+              <PublicRoute path="/login" component={Login} />
+              <PublicRoute path="/signup" component={Signup} />
+              <PublicRoute exact path="/" component={Home} />
+              <PublicRoute component={NotFound} />
             </Switch>
             <Footer />
           </div>
---
+++ src/views/UserDetails.js
@@ -0,0 +1,5 @@
+const UserDetails = () => {
+  return <h1>userdetails</h1>;
+};
+
+export default UserDetails;
