9ef5efe5adfa5c87c27d3f24c4428236be81fdeb
ericbsantana <bitencourteric@hotmail.com>
2021-07-26 09:57:45 -0300

    add button custom class customizer

--- src/components/layout/Form/Button.js
+++ src/components/layout/Form/Button.js
@@ -2,7 +2,7 @@ const Button = (props) => {
   return (
     <button
       type={props.type}
-      className="w-full text-center font-bold uppercase py-4 rounded bg-yellow-300 hover:bg-yellow-400 focus:outline-none focus:ring-2 focus:ring-yellow-600 focus:ring-opacity-50"
+      className={`w-full text-center font-bold uppercase py-4 rounded bg-yellow-300 hover:bg-yellow-400 focus:outline-none focus:ring-2 focus:ring-yellow-600 focus:ring-opacity-50 ${props.className}`}
     >
       {props.children}
     </button>
